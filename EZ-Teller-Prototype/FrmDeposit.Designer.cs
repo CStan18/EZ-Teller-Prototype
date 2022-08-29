42bcfabf245ad911181232d41fe5256dab96a79a
Caleb Standridge <99684824+CStan18@users.noreply.github.com>
2022-08-29 11:08:44 -0500

    Adjust deposit layout, add Amount keypress event Key press event checks if input is num

---
+++ EZ-Teller-Prototype/FrmDeposit.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace EZ_Teller_Prototype
+{
+    partial class FrmDeposit
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblAmount = new System.Windows.Forms.Label();
+            this.txtAmount = new System.Windows.Forms.TextBox();
+            this.txtCash = new System.Windows.Forms.TextBox();
+            this.lblCash = new System.Windows.Forms.Label();
+            this.txtCheck = new System.Windows.Forms.TextBox();
+            this.lvlCheck = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblAmount
+            // 
+            this.lblAmount.AutoSize = true;
+            this.lblAmount.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAmount.Location = new System.Drawing.Point(103, 84);
+            this.lblAmount.Name = "lblAmount";
+            this.lblAmount.Size = new System.Drawing.Size(71, 20);
+            this.lblAmount.TabIndex = 0;
+            this.lblAmount.Text = "Amount";
+            // 
+            // txtAmount
+            // 
+            this.txtAmount.Location = new System.Drawing.Point(204, 84);
+            this.txtAmount.Name = "txtAmount";
+            this.txtAmount.Size = new System.Drawing.Size(151, 20);
+            this.txtAmount.TabIndex = 1;
+            this.txtAmount.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtAmount_KeyPress);
+            // 
+            // txtCash
+            // 
+            this.txtCash.Location = new System.Drawing.Point(204, 124);
+            this.txtCash.Name = "txtCash";
+            this.txtCash.Size = new System.Drawing.Size(151, 20);
+            this.txtCash.TabIndex = 3;
+            // 
+            // lblCash
+            // 
+            this.lblCash.AutoSize = true;
+            this.lblCash.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCash.Location = new System.Drawing.Point(103, 124);
+            this.lblCash.Name = "lblCash";
+            this.lblCash.Size = new System.Drawing.Size(50, 20);
+            this.lblCash.TabIndex = 2;
+            this.lblCash.Text = "Cash";
+            // 
+            // txtCheck
+            // 
+            this.txtCheck.Location = new System.Drawing.Point(204, 167);
+            this.txtCheck.Name = "txtCheck";
+            this.txtCheck.Size = new System.Drawing.Size(151, 20);
+            this.txtCheck.TabIndex = 5;
+            // 
+            // lvlCheck
+            // 
+            this.lvlCheck.AutoSize = true;
+            this.lvlCheck.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lvlCheck.Location = new System.Drawing.Point(103, 167);
+            this.lvlCheck.Name = "lvlCheck";
+            this.lvlCheck.Size = new System.Drawing.Size(59, 20);
+            this.lvlCheck.TabIndex = 4;
+            this.lvlCheck.Text = "Check";
+            // 
+            // FrmDeposit
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtCheck);
+            this.Controls.Add(this.lvlCheck);
+            this.Controls.Add(this.txtCash);
+            this.Controls.Add(this.lblCash);
+            this.Controls.Add(this.txtAmount);
+            this.Controls.Add(this.lblAmount);
+            this.Name = "FrmDeposit";
+            this.Text = "FrmDeposit";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblAmount;
+        private System.Windows.Forms.TextBox txtAmount;
+        private System.Windows.Forms.TextBox txtCash;
+        private System.Windows.Forms.Label lblCash;
+        private System.Windows.Forms.TextBox txtCheck;
+        private System.Windows.Forms.Label lvlCheck;
+    }
+}
---
+++ EZ-Teller-Prototype/FrmDeposit.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EZ_Teller_Prototype
+{
+    public partial class FrmDeposit : Form
+    {
+        public FrmDeposit()
+        {
+            InitializeComponent();
+        }
+
+        private void txtAmount_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = General.isNumber(e);
+        }
+    }
+}
