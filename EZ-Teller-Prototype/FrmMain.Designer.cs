cb3fd14637ed71d2e9fbac76d215586d7cdbac46
Caleb Standridge <99684824+CStan18@users.noreply.github.com>
2022-08-29 10:54:15 -0500

    Construct main form layout

---
+++ EZ-Teller-Prototype/FrmMain.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace EZ_Teller_Prototype
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lnkDeposit = new System.Windows.Forms.LinkLabel();
+            this.lnkCloseAccount = new System.Windows.Forms.LinkLabel();
+            this.lnkLookUp = new System.Windows.Forms.LinkLabel();
+            this.lnkLoanPmnt = new System.Windows.Forms.LinkLabel();
+            this.lnkMortgagePmnt = new System.Windows.Forms.LinkLabel();
+            this.lnkWithdrawal = new System.Windows.Forms.LinkLabel();
+            this.lnkBuyIn = new System.Windows.Forms.LinkLabel();
+            this.lnkOpenAccount = new System.Windows.Forms.LinkLabel();
+            this.lnkSell = new System.Windows.Forms.LinkLabel();
+            this.lnkBalance = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // lnkDeposit
+            // 
+            this.lnkDeposit.AutoSize = true;
+            this.lnkDeposit.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lnkDeposit.Location = new System.Drawing.Point(73, 47);
+            this.lnkDeposit.Name = "lnkDeposit";
+            this.lnkDeposit.Size = new System.Drawing.Size(103, 24);
+            this.lnkDeposit.TabIndex = 0;
+            this.lnkDeposit.TabStop = true;
+            this.lnkDeposit.Text = "1. Deposit";
+            // 
+            // lnkCloseAccount
+            // 
+            this.lnkCloseAccount.AutoSize = true;
+            this.lnkCloseAccount.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lnkCloseAccount.Location = new System.Drawing.Point(322, 95);
+            this.lnkCloseAccount.Name = "lnkCloseAccount";
+            this.lnkCloseAccount.Size = new System.Drawing.Size(169, 24);
+            this.lnkCloseAccount.TabIndex = 1;
+            this.lnkCloseAccount.TabStop = true;
+            this.lnkCloseAccount.Text = "7. Close Account";
+            // 
+            // lnkLookUp
+            // 
+            this.lnkLookUp.AutoSize = true;
+            this.lnkLookUp.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lnkLookUp.Location = new System.Drawing.Point(73, 239);
+            this.lnkLookUp.Name = "lnkLookUp";
+            this.lnkLookUp.Size = new System.Drawing.Size(111, 24);
+            this.lnkLookUp.TabIndex = 2;
+            this.lnkLookUp.TabStop = true;
+            this.lnkLookUp.Text = "5. Look-Up";
+            // 
+            // lnkLoanPmnt
+            // 
+            this.lnkLoanPmnt.AutoSize = true;
+            this.lnkLoanPmnt.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lnkLoanPmnt.Location = new System.Drawing.Point(73, 191);
+            this.lnkLoanPmnt.Name = "lnkLoanPmnt";
+            this.lnkLoanPmnt.Size = new System.Drawing.Size(132, 24);
+            this.lnkLoanPmnt.TabIndex = 3;
+            this.lnkLoanPmnt.TabStop = true;
+            this.lnkLoanPmnt.Text = "4. Loan Pmnt";
+            // 
+            // lnkMortgagePmnt
+            // 
+            this.lnkMortgagePmnt.AutoSize = true;
+            this.lnkMortgagePmnt.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lnkMortgagePmnt.Location = new System.Drawing.Point(73, 143);
+            this.lnkMortgagePmnt.Name = "lnkMortgagePmnt";
+            this.lnkMortgagePmnt.Size = new System.Drawing.Size(174, 24);
+            this.lnkMortgagePmnt.TabIndex = 4;
+            this.lnkMortgagePmnt.TabStop = true;
+            this.lnkMortgagePmnt.Text = "3. Mortgage Pmnt";
+            // 
+            // lnkWithdrawal
+            // 
+            this.lnkWithdrawal.AutoSize = true;
+            this.lnkWithdrawal.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lnkWithdrawal.Location = new System.Drawing.Point(73, 95);
+            this.lnkWithdrawal.Name = "lnkWithdrawal";
+            this.lnkWithdrawal.Size = new System.Drawing.Size(135, 24);
+            this.lnkWithdrawal.TabIndex = 5;
+            this.lnkWithdrawal.TabStop = true;
+            this.lnkWithdrawal.Text = "2. Withdrawal";
+            // 
+            // lnkBuyIn
+            // 
+            this.lnkBuyIn.AutoSize = true;
+            this.lnkBuyIn.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lnkBuyIn.Location = new System.Drawing.Point(322, 143);
+            this.lnkBuyIn.Name = "lnkBuyIn";
+            this.lnkBuyIn.Size = new System.Drawing.Size(145, 24);
+            this.lnkBuyIn.TabIndex = 6;
+            this.lnkBuyIn.TabStop = true;
+            this.lnkBuyIn.Text = "8. Buy-in Cash";
+            // 
+            // lnkOpenAccount
+            // 
+            this.lnkOpenAccount.AutoSize = true;
+            this.lnkOpenAccount.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lnkOpenAccount.Location = new System.Drawing.Point(322, 47);
+            this.lnkOpenAccount.Name = "lnkOpenAccount";
+            this.lnkOpenAccount.Size = new System.Drawing.Size(168, 24);
+            this.lnkOpenAccount.TabIndex = 7;
+            this.lnkOpenAccount.TabStop = true;
+            this.lnkOpenAccount.Text = "6. Open Account";
+            // 
+            // lnkSell
+            // 
+            this.lnkSell.AutoSize = true;
+            this.lnkSell.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lnkSell.Location = new System.Drawing.Point(322, 191);
+            this.lnkSell.Name = "lnkSell";
+            this.lnkSell.Size = new System.Drawing.Size(121, 24);
+            this.lnkSell.TabIndex = 8;
+            this.lnkSell.TabStop = true;
+            this.lnkSell.Text = "9. Sell Cash";
+            // 
+            // lnkBalance
+            // 
+            this.lnkBalance.AutoSize = true;
+            this.lnkBalance.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lnkBalance.Location = new System.Drawing.Point(322, 239);
+            this.lnkBalance.Name = "lnkBalance";
+            this.lnkBalance.Size = new System.Drawing.Size(108, 24);
+            this.lnkBalance.TabIndex = 9;
+            this.lnkBalance.TabStop = true;
+            this.lnkBalance.Text = "0. Balance";
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(582, 322);
+            this.Controls.Add(this.lnkBalance);
+            this.Controls.Add(this.lnkSell);
+            this.Controls.Add(this.lnkOpenAccount);
+            this.Controls.Add(this.lnkBuyIn);
+            this.Controls.Add(this.lnkWithdrawal);
+            this.Controls.Add(this.lnkMortgagePmnt);
+            this.Controls.Add(this.lnkLoanPmnt);
+            this.Controls.Add(this.lnkLookUp);
+            this.Controls.Add(this.lnkCloseAccount);
+            this.Controls.Add(this.lnkDeposit);
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "EZ Teller (prototype)";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.LinkLabel lnkDeposit;
+        private System.Windows.Forms.LinkLabel lnkCloseAccount;
+        private System.Windows.Forms.LinkLabel lnkLookUp;
+        private System.Windows.Forms.LinkLabel lnkLoanPmnt;
+        private System.Windows.Forms.LinkLabel lnkMortgagePmnt;
+        private System.Windows.Forms.LinkLabel lnkWithdrawal;
+        private System.Windows.Forms.LinkLabel lnkBuyIn;
+        private System.Windows.Forms.LinkLabel lnkOpenAccount;
+        private System.Windows.Forms.LinkLabel lnkSell;
+        private System.Windows.Forms.LinkLabel lnkBalance;
+    }
+}
---
+++ EZ-Teller-Prototype/FrmMain.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EZ_Teller_Prototype
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
