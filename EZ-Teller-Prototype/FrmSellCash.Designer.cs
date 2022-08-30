e79ab7da2cbfd9186f5861b3eafa9530d754841d
Caleb Standridge <99684824+CStan18@users.noreply.github.com>
2022-08-30 09:54:33 -0500

    Add mortgage, withdrawal, buy cash, sell cash frms

---
+++ EZ-Teller-Prototype/FrmSellCash.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace EZ_Teller_Prototype
+{
+    partial class FrmSellCash
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnDone = new System.Windows.Forms.Button();
+            this.txtCash = new System.Windows.Forms.TextBox();
+            this.lblTellerID = new System.Windows.Forms.Label();
+            this.txtAmount = new System.Windows.Forms.TextBox();
+            this.lblAmount = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnClose
+            // 
+            this.btnClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClose.Location = new System.Drawing.Point(184, 124);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(121, 38);
+            this.btnClose.TabIndex = 3;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnDone
+            // 
+            this.btnDone.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDone.Location = new System.Drawing.Point(30, 124);
+            this.btnDone.Name = "btnDone";
+            this.btnDone.Size = new System.Drawing.Size(121, 38);
+            this.btnDone.TabIndex = 2;
+            this.btnDone.Text = "Done";
+            this.btnDone.UseVisualStyleBackColor = true;
+            // 
+            // txtCash
+            // 
+            this.txtCash.Location = new System.Drawing.Point(127, 68);
+            this.txtCash.Name = "txtCash";
+            this.txtCash.Size = new System.Drawing.Size(151, 20);
+            this.txtCash.TabIndex = 1;
+            // 
+            // lblTellerID
+            // 
+            this.lblTellerID.AutoSize = true;
+            this.lblTellerID.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTellerID.Location = new System.Drawing.Point(26, 68);
+            this.lblTellerID.Name = "lblTellerID";
+            this.lblTellerID.Size = new System.Drawing.Size(77, 20);
+            this.lblTellerID.TabIndex = 37;
+            this.lblTellerID.Text = "Teller ID";
+            // 
+            // txtAmount
+            // 
+            this.txtAmount.Location = new System.Drawing.Point(127, 28);
+            this.txtAmount.Name = "txtAmount";
+            this.txtAmount.Size = new System.Drawing.Size(151, 20);
+            this.txtAmount.TabIndex = 0;
+            // 
+            // lblAmount
+            // 
+            this.lblAmount.AutoSize = true;
+            this.lblAmount.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAmount.Location = new System.Drawing.Point(26, 28);
+            this.lblAmount.Name = "lblAmount";
+            this.lblAmount.Size = new System.Drawing.Size(71, 20);
+            this.lblAmount.TabIndex = 36;
+            this.lblAmount.Text = "Amount";
+            // 
+            // FrmSellCash
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(331, 172);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnDone);
+            this.Controls.Add(this.txtCash);
+            this.Controls.Add(this.lblTellerID);
+            this.Controls.Add(this.txtAmount);
+            this.Controls.Add(this.lblAmount);
+            this.Name = "FrmSellCash";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Sell Cash";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnDone;
+        private System.Windows.Forms.TextBox txtCash;
+        private System.Windows.Forms.Label lblTellerID;
+        private System.Windows.Forms.TextBox txtAmount;
+        private System.Windows.Forms.Label lblAmount;
+    }
+}
